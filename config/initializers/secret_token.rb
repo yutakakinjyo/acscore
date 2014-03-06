d8fad2f06a749e41a5c5caf1f9806dbfee086b60
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-06 21:36:06 +0900

    rails new acscore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acscore::Application.config.secret_key_base = 'fc150b38e2d77a5c1eec3cd957617fa7889983b103ba956cf294371cab4c1bd38dec409f8712cd0614563d505244015d94a8f70d9418114de5cfe33b4b75c5eb'
