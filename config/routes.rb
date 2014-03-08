b4ed86f4d355bd3722e793cdfe87ed043b5ac704
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-08 23:19:19 +0900

    rails g scaffold Score user_id:integer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Acscore::Application.routes.draw do
+  resources :scores
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
