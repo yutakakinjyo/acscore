8ef3af624d2fb7c1785682e079a7d93e7155112d
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-09 19:21:36 +0900

    現在日付から一ヶ月前までのカレンダーを表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,16 @@ class HomeController < ApplicationController
 
   def index
     @user = current_user
+    # TODO: form_for の引数で必要だが、controller でnewできるので、消し去りたい. form_for を引数なしでできればいい
     @score = Score.new
     @scores = @user.scores
+    @now = Time.now
+    @last_month = @now.last_month
+    @da = []
+    while @last_month != @now do
+      @da.push(@last_month)
+      @last_month = @last_month.tomorrow
+    end
+
   end
 end
