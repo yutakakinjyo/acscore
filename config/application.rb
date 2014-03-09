082cd793d15299f2bda2766582d1b2a6b45eeeab
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-09 15:14:46 +0900

    config.active_record.default_timezone = :loacl にすると画面に表示されなくなった。コメントアウトで問題なく表示されるので、コメントアウトする

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Acscore
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Tokyo'
-    config.active_record.defalut_timezone = :loacl
+    # config.active_record.default_timezone = :loacl
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
