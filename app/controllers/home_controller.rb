f10fd4698c967f61b5092802f4228112476d9828
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-09 21:47:22 +0900

    add score for date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,12 @@ class HomeController < ApplicationController
     @scores = @user.scores
     @now = Time.now
     @last_month = @now.last_month
-    @da = []
-    while @last_month != @now do
-      @da.push(@last_month)
+    @date_array = []
+    while @last_month != @now.tomorrow do
+      @size = @scores.where(created_at: @last_month.beginning_of_day..@last_month.end_of_day).size
+      @hash = {"date" => @last_month, "score" => @size}
+      @date_array.push(@hash)
       @last_month = @last_month.tomorrow
     end
-
   end
 end
