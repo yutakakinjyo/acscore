447117dd56fec47f743775558ddd3bb96e26c5bf
Yutaka Kinjo <yutakakinjyo@gmail.com>
2014-03-11 17:51:00 +0900

    @nowのスコープを変更 nowに

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,14 @@
+# -*- coding: utf-8 -*-
 class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
     @user = current_user
-    # TODO: form_for の引数で必要だが、controller でnewできるので、消し去りたい. form_for を引数なしでできればいい
-    @score = Score.new
     @scores = @user.scores
-    @now = Time.now
-    @last_month = @now.last_month
+    now = Time.now
+    @last_month = now.last_month
     @date_array = []
-    while @last_month != @now.tomorrow do
+    while @last_month != now.tomorrow do
       @size = @scores.where(created_at: @last_month.beginning_of_day..@last_month.end_of_day).size
       if @size > 30 then
         @color = "green"
