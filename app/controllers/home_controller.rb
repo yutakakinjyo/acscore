d5e8205038066492bfc834dd5ecb12da3a14ee8e
Yutaka Kinjo <yutakakinjyo@gmail.com>
2014-03-11 18:06:14 +0900

    変数のスコープをインスタンス変数からローカル変数に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,20 +5,20 @@ class HomeController < ApplicationController
   def index
     @scores = current_user.scores
     now = Time.now
-    @last_month = now.last_month
+    current_month = now.last_month
     @date_array = []
-    while @last_month != now.tomorrow do
-      @size = @scores.where(created_at: @last_month.beginning_of_day..@last_month.end_of_day).size
-      if @size > 30 then
-        @color = "green"
-      elsif @size >= 1 then
-        @color = "lightgreen"
+    while current_month  != now.tomorrow do
+      size = @scores.where(created_at: current_month.beginning_of_day..current_month.end_of_day).size
+      if size > 30 then
+        color = "green"
+      elsif size >= 1 then
+        color = "lightgreen"
       else
-        @color = "lightgray"
+        color = "lightgray"
       end
-      @hash = {"date" => @last_month, "score" => @size, "color" => @color}
+      @hash = {"date" => current_month, "score" => size, "color" => color}
       @date_array.push(@hash)
-      @last_month = @last_month.tomorrow
+      current_month = current_month.tomorrow
     end
   end
 end
