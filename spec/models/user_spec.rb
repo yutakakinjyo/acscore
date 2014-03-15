cf96ef518e535dc2c2a0eb7bd4c17158bde77c51
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-15 23:41:02 +0900

    my first rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "total_score" do
+    before do
+      @user = User.new
+      score = score.new
+      score.user_id = @user.id
+      @user.save
+      score.save
+    end
+    context "get total score" do
+      it "total score is 1" do
+       @user.total_score.should 1
+      end
+    end
+  end
 end
