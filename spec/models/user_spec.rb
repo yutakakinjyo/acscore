e148619d781b95c95c00789abf39cb8eda06ef27
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-16 14:33:39 +0900

    score calendar のテストを追加。user が score を持っていない場合の 一番初めの日付と一番終わりの日付の値をチェックする境界値チェックを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe User do
 
   describe "total_score" do
-
     before do
       @user = User.new
       @user.save(validate: false)
@@ -19,4 +18,22 @@ describe User do
       end
     end
   end
+
+  describe "score_calendar" do
+    before do
+      @user = User.new
+      @user.save(validate: false)
+      @time_now = Time.local(1987,5,9,0,0,0)
+      Time.stub!(:now).and_return(@time_now)
+    end
+
+    context "call score_calendar" do
+      it "has zero socre" do
+        expect(@user.score_calendar.first).to eq({"date"=> @time_now.last_month, "color" => "lightgray"})
+        expect(@user.score_calendar.last).to eq({"date"=> @time_now, "color" => "lightgray"})
+      end
+    end
+  end
+
 end
+
