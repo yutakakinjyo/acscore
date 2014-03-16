f3c86db538ba8cccce744948f98ee1389b275533
Yutaka Kinjo <yutakakinkyo@gmail.com>
2014-03-16 13:19:00 +0900

    add socre zero case test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe User do
+
   describe "total_score" do
+
     before do
       @user = User.new
       @user.save(validate: false)
-      @user.scores.create
     end
+
     context "call total score" do
       it "has one socre" do
-       expect(@user.total_score).to eq(1)
+        @user.scores.create
+        expect(@user.total_score).to eq(1)
+      end
+      it "has zero socre" do
+        expect(@user.total_score).to eq(0)
       end
     end
   end
